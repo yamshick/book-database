f28a6e54485ca7abbbf9158b4f076e91e99f6acc
username <myfancyemail@gmail.com>
2023-06-03 10:59:04 +0300

    reader searcher

--- reader/manager.py
+++ reader/manager.py
@@ -1,15 +1,26 @@
 import database
+import searcher
 
 def manage():
     # db_path = '/media/danchick/7848-BA38/Рассказ.db'
     db_path = '/home/danchick/librarian/book-database/real-books/Рассказ.db'
     # db_path = '/home/danchick/librarian/book-database/Рассказ.db'
     sub_string = 'термос'
+    # fragment_left_offset = 100
+    # fragment_right_offset = 100
     # sub_string = 'а'
     try:
         data = database.select_books_info_by_substring(db_path, sub_string)
         # data = database.search_books_info_by_substring(db_path, sub_string)
         print(len(data))
+        for book_info in data:
+            book_text = book_info[5]
+            print(len(book_text))
+            book_fragments = searcher.get_book_fragments_with_substring(book_text, sub_string)
+            print("book_fragments_len:", len(book_fragments))
+            for fragment in book_fragments:
+                print(fragment.strip(), "\n")
+                # print(len(fragment))
     except Exception as e:
         print(e)
              
--- reader/searcher.py
+++ reader/searcher.py
@@ -2,8 +2,24 @@ import re
 
 def get_list_of_substring_in_book(sub, book):
     try:
-        print(book)
+        # print(book)
         subs_index_list = [m.start() for m in re.finditer(sub, book)]
         return subs_index_list
     except Exception as e:
-        print("get_list_of_substring_in_book error", e)   
+        print("get_list_of_substring_in_book error", e)
+
+
+def get_book_fragments_with_substring(book, sub_string, fragment_left_offset = 50, fragment_right_offset = 50):
+    try:
+        subs_index_list = get_list_of_substring_in_book(sub_string, book)
+        fragments = []
+        # print("subs_index_list:", subs_index_list)
+        for sub_start in subs_index_list:
+            left = max(sub_start - fragment_left_offset, 0)
+            right = min(sub_start + fragment_right_offset, len(book))
+            fragment = book[left:right]
+            # fragment = book.slice(left,right)
+            fragments.append(fragment)
+        return fragments 
+    except Exception as e:
+        print("get_book_fragments_with_substring error", e)
